aaa5e121e649ae6ee433c772ee6efe11288fce35
Victor Ponce <vicmanuelr@gmail.com>
2023-01-30 11:00:17 -0600

    Created new template from html5up
    Added card elements(css/images/js/html)

--- server.py
+++ server.py
@@ -1,15 +1,14 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return '<h1>Hello World!</h1>' \
-
+def home():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
     # Run the app in debug mode to auto-reload/debug
     app.run(debug=True)
-    
